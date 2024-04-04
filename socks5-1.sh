e56043a2fe7d85ec8767d36ab02ac7c1ad9d0916
ItsZhenka <106002482+ItsZhenka@users.noreply.github.com>
2024-04-05 03:23:53 +0700

    Create socks5-1.sh

---
+++ socks5-1.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+echo -e "Please enter the username for the socks5 proxy:"
+read username
+echo -e "Please enter the password for the socks5 proxy:"
+read -s password
+
+# Update repositories
+sudo apt update -y
+
+# Install dante-server
+sudo apt install dante-server -y
+
+# Create the configuration file
+sudo bash -c 'cat <<EOF > /etc/danted.conf
+logoutput: syslog
+user.privileged: root
+user.unprivileged: nobody
+
+# The listening network interface or address.
+internal: 0.0.0.0 port=1080
+
+# The proxying network interface or address.
+external: ens3
+
+# socks-rules determine what is proxied through the external interface.
+socksmethod: username
+
+# client-rules determine who can connect to the internal interface.
+clientmethod: none
+
+client pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}
+
+socks pass {
+    from: 0.0.0.0/0 to: 0.0.0.0/0
+}
+EOF'
+
+# Add user with password
+sudo useradd --shell /usr/sbin/nologin $username
+echo "$username:$password" | sudo chpasswd
+
+# Check if UFW is active and open port 1080 if needed
+if sudo ufw status | grep -q "Status: active"; then
+    sudo ufw allow 1080/tcp
+fi
+
+# Check if iptables is active and open port 1080 if needed
+if sudo iptables -L | grep -q "ACCEPT     tcp  --  anywhere             anywhere             tcp dpt:1080"; then
+    echo "Port 1080 is already open in iptables."
+else
+    sudo iptables -A INPUT -p tcp --dport 1080 -j ACCEPT
+fi
+
+# Restart dante-server
+sudo systemctl restart danted
+
+# Enable dante-server to start at boot
+sudo systemctl enable danted
